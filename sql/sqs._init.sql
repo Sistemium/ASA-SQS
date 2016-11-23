442f20dcad1c0376e2a7c77cac231e375477254a
Alexander Levin <alevin.ru@gmail.com>
2016-11-23 17:19:12 +0200

    rm settings

--- sql/sqs._init.sql
+++ sql/sqs._init.sql
@@ -18,11 +18,6 @@ meta.createTable 'sqs.Msg', 0, 1;
 
 create index XK_sqs_Msg_isDelivered_cts on sqs.Msg (isDelivered, cts);
 
-util.setUserOption 'sqs.processor.url.r50d', 'http://aws.sistemium.net/execute-api/gozy5p0624/prod';
-util.setUserOption 'sqs.processor.headers.r50d', 'x-api-key:BXAjWM62wD4zPVYpAXZni8QhIgGojakx7fE2dUk3';
-
-commit
-
 */
 
 create or replace function sqs.post(
